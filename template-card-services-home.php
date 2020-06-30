643eab1d7562226c9fc4870e4097f49c73ba92f1
Mike Setzer <michaelsetzerucf@gmail.com>
2020-06-30 16:57:52 -0400

    Update template-card-services-home.php
    I hate my job

--- template-card-services-home.php
+++ template-card-services-home.php
@@ -223,8 +223,8 @@ Template Name: Card Services Home
 
                                         <div class="post-content">
                                             <?php
-                                            if ( strlen( $post->post_content ) > 70 ) {
-                                                echo substr( $post->post_content, 0, 70 ) . '...';
+                                            if ( strlen( $post->post_content ) > 50 ) {
+                                                echo substr( $post->post_content, 0, 50 ) . '...';
                                             } else {
                                                 echo $post->post_content;
                                             }
