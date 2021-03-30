b070cc91dd3861c5021738886a76c64258a4d40b
Mike Setzer <michaelsetzerucf@gmail.com>
2021-03-30 11:35:49 -0400

    Revert "Update templates-departments.php"
    This reverts commit 6013005956c78637e44986d716891f2fea4d45b8.

--- templates-departments.php
+++ templates-departments.php
@@ -163,6 +163,7 @@ Template Name: Departments
 
 				//Clear Form Errors
 				$('#department-form').find('.error').removeClass('error');
+
 				$('#department-form').trigger("reset");
 
 				//Change Selected Nav item class type to active
@@ -214,7 +215,7 @@ Template Name: Departments
 
 		} );
 
-        $('#department-form').keyup(gform_input_change);
+        $('#department-form').on( 'keyup', function(gform_input_change));
 
         $('#input_6_4, #input_8_4, #input_3_4, select').fancySelect();
 
